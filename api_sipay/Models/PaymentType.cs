f5313b05c83cb2802ef53048121b6fde0e1ee181
Gülsen Keskin <63197899+gulsenkeskin@users.noreply.github.com>
2022-08-10 06:14:16 +0300

    Create PaymentType.cs

---
+++ api_sipay/Models/PaymentType.cs
@@ -0,0 +1,10 @@
+﻿namespace ApiSipay.Models
+{
+    public enum PaymentType : int
+    {
+        WhiteLabel2D = 0,
+        WhiteLabel2DOr3D = 1,
+        WhiteLabel3D = 2,
+        BrandedPayment = 4
+    }
+}
