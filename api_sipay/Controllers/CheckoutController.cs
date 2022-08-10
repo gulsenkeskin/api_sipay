e4d6c79a8ae77304626b61237482291f1bf456b9
Gülsen Keskin <63197899+gulsenkeskin@users.noreply.github.com>
2022-08-10 05:46:28 +0300

    Update CheckoutController.cs

--- api_sipay/Controllers/CheckoutController.cs
+++ api_sipay/Controllers/CheckoutController.cs
@@ -1,9 +1,24 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 
 namespace ApiSipay.Controllers
 {
     public class CheckoutController : Controller
     {
+        private readonly ILogger<CheckoutController> _logger;
+        private readonly IConfiguration _config;
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public CheckoutController(ILogger<CheckoutController> logger, IConfiguration config,
+   IHttpContextAccessor httpContextAccessor)
+        {
+            _logger = logger;
+            _config = config;
+            _httpContextAccessor = httpContextAccessor;
+        } 
+
         public IActionResult Index()
         {
             return View();
