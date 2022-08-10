12814213f283cbc51e1c5cf9409867e2c1564eeb
Gülsen Keskin <63197899+gulsenkeskin@users.noreply.github.com>
2022-08-10 05:53:13 +0300

    Create CheckoutModel.cs

---
+++ api_sipay/Models/CheckoutModel.cs
@@ -0,0 +1,12 @@
+﻿namespace ApiSipay.Models
+{
+    public class CheckoutModel
+    {
+        public string CCHolderName { private get; set; }
+        public string CCNo { private get; set; }
+        public string ExpiryMonth { private get; set; }
+        public string ExpiryYear { private get; set; }
+        public string CCV { private get; set; }
+
+    }
+}
